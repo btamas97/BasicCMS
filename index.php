07939cd4d28ad7c1a01ec1f2d2752675a4b12d92
btamas97 <btamas97@users.noreply.github.com>
2018-01-21 20:02:35 +0100

    Nav bar and login window added
    Inside the nav bar site logo and Contribute ref which opens the login window

--- index.php
+++ index.php
@@ -10,6 +10,48 @@
     <title> Basic CMS </title>
   </head>
   <body>
-    
+    <div id="header" name="header">
+      <div id="logo_name">
+        <a class="din-normal logo">Basic<p class="din-bold logo">CMS</a><p class="din-light logo version">v0.1</p>
+      </div>
+      <div id="login">
+        <a id='contribute' class='din-normal contribute'>Contribute</a>
+      </div>
+    </div>
+    <div id="LoginWindow">
+      <div class="wrapper">
+        <form action=index.php method="post" autocomplete="off" >
+          <div class="group">
+            <input type="text" name="uname" required="required"/><span class="highlight"></span><span class="bar"></span>
+            <label>Username</label>
+          </div>
+          <div class="group">
+            <input type="password" name="pwd" required="required"/><span class="highlight"></span><span class="bar"></span>
+            <label>Password</label>
+          </div>
+          <div class="btn-box">
+            <button class="btn btn-submit" name="btn_login" type="submit">Login</button>
+            <button class="btn btn-cancel" name="btn_register" type="submit">Registration</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </body>
+  <script>
+    document.getElementById("contribute").onclick = showLogin;
+
+    var isShown = false;
+    function showLogin() {
+      if (isShown == false) {
+        document.getElementById('LoginWindow').style.display = "block";
+        isShown = true;
+        exit();
+      }
+
+    if (isShown == true) {
+      document.getElementById('LoginWindow').style.display = "none";
+      isShown = false;
+    }
+  }
+</script>
 </html>
