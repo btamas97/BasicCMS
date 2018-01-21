a18acfb8092a0545ec6be28ff97f357d0090f19c
btamas97 <btamas97@users.noreply.github.com>
2018-01-21 19:30:08 +0100

    Index php database connection and header information added

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ include_once('connect.php');
+ ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Basic CMS">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <title> Basic CMS </title>
+  </head>
+  <body>
+    
+  </body>
+</html>
