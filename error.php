948d07abbe0a06f194195d8dfe86833b099db1fd
btamas97 <btamas97@users.noreply.github.com>
2018-01-26 02:54:12 +0100

    error message made visually more appealing
    added some css and html to error.php

--- error.php
+++ error.php
@@ -1,4 +1,20 @@
 <?php
     session_start();
-    echo $_SESSION['message'];
 ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Basic CMS">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <title> Basic CMS </title>
+  </head>
+  <body>
+    <div id="content">
+      <div id="article">
+        <p class="din-bold Title"> An Error Occured: </p>
+        <p class="din-bold"> <?php echo $_SESSION['message']; ?> </p>
+        <small><a href="index.php"> Vissza a kezdőlapra </a></small>
+      </div>
+    </div>
+  </body>
